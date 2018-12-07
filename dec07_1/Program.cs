a0d805adac4e5d337b2c117cf8fecaef9b88e184
Eric Minkes <eric.minkes@gmail.com>
2018-12-07 11:10:04 +0100

    Day 7 part 1

---
+++ dec07_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace dec07_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instructions = System.IO.File.ReadAllLines("input.txt")
+                .Select(l => l.Split(' '))
+                .Select(l => new { In = l[1], Out = l[7] })
+                .ToList();
+
+            instructions = instructions.Concat(instructions.Select(i => i.Out).Where(c => !instructions.Select(i => i.In).Contains(c))
+                .Distinct().Select(c => new { In = c, Out = "" })).ToList();
+
+            var order = "";
+            while (instructions.Any())
+            {
+                var step = instructions.Select(i => i.In).Where(c => !instructions.Select(i => i.Out).Contains(c))
+                    .OrderBy(c => c).First();
+                order += step;
+                instructions = instructions.Where(i => i.In != step).ToList();
+            }
+            Console.WriteLine(order);
+            Console.ReadLine();
+        }
+    }
+}
