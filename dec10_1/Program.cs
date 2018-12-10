e17fd11ae22db9c3b544d90f33013aab1dd34775
Eric Minkes <eric.minkes@gmail.com>
2018-12-10 10:42:05 +0100

    Day 10

---
+++ dec10_1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dec10_1
+{
+    class Program
+    {
+        class Point
+        {
+            public int X;
+            public int Y;
+            public int dX;
+            public int dY;
+        }
+
+        static void Main(string[] args)
+        {
+            var points = new List<Point>();
+            foreach (var line in System.IO.File.ReadAllLines("input.txt"))
+            {
+                var parts = line.Split("> velocity=<");
+                var parts1 = parts[0].Split(',');
+                var x = int.Parse(parts1[0].Substring("position=<".Length));
+                var y = int.Parse(parts1[1]);
+                var parts2 = parts[1].Split(',');
+                var dx = int.Parse(parts2[0]);
+                var dy = int.Parse(parts2[1].Substring(0, parts2[1].Length - 1));
+                var point = new Point { X = x, Y = y, dX = dx, dY = dy };
+                points.Add(point);
+            }
+            var minBound = points.Max(p => p.X) - points.Min(p => p.X) + points.Max(p => p.Y) - points.Min(p => p.Y);
+            for (int t = 0; t < 1000000; t++)
+            {
+                foreach (var point in points)
+                {
+                    point.X += point.dX;
+                    point.Y += point.dY;
+                }
+                var bound = points.Max(p => p.X) - points.Min(p => p.X) + points.Max(p => p.Y) - points.Min(p => p.Y);
+                if (bound < minBound)
+                {
+                    minBound = bound;
+                }
+                else
+                {
+                    foreach (var point in points)
+                    {
+                        point.X -= point.dX;
+                        point.Y -= point.dY;
+                    }
+                    for (int y = points.Min(p => p.Y); y <= points.Max(p => p.Y); y++)
+                    {
+                        for (int x = points.Min(p => p.X); x <= points.Max(p => p.X); x++)
+                        {
+                            Console.Write(points.Any(p => p.X == x && p.Y == y) ? "#" : ".");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
